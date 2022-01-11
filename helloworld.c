03eb655ee71b8da64f233ef6fef4cee3dbab9091
Gustavo Henrique <69666481+DjdogeGamer@users.noreply.github.com>
2022-01-11 18:22:04 -0300

    Add files via upload

---
+++ helloworld.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+//comentario
+
+/* 
+a
+a
+a
+*/
+
+int main(){
+    int num = 0;
+    int cubo = 0;
+
+    printf("Digite um numero: ");
+    scanf("%d", &num);
+
+    cubo=num*num*num;
+    printf("Cubo de %d = %d\n",num,cubo);
+
+    return 0;
+}
+
